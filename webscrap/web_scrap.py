271bd9a9ea3a133243fb937ae6dea0052187247c
hongkai0203 <hongkai0203@gmail.com>
2022-11-13 04:24:49 -0600

    Got the extensive list of all the courses

--- webscrap/web_scrap.py
+++ webscrap/web_scrap.py
@@ -4,16 +4,34 @@ import requests
 def get_majors_links(url):
     result = requests.get(url)
     doc = BeautifulSoup(result.text, "html.parser")
-    print(doc)
+    #print(doc)
 
     #Writing output to txtfile 
     htmlOutput = doc.prettify
     with open('htmlOutput2.txt', 'a') as f:
         f.write(str(htmlOutput))
+    #TODO : delete this thing above 
+
+    table = doc.find(["div"], class_="visual-sitemap grid")
+    children = table.findChildren("a") #Children node here gives me the entire HTML code of the list of majors 
+    link_major_pair ={}
+    for x in children:
+        if "B.S." not in x.string:
+            #print(x["href"])
+            #print(x.string)
+            #print()
+            link_major_pair[x.string] = x["href"]
+    #print(link_major_pair["Zoology, B.A."])
+    #table = doc.find_all(["ul"])
+    #print(children)
+    return link_major_pair
+
+
+
 
     
 
-def get_courses(url):
+def get_courses(major, url):
     result = requests.get(url) #gets the stuff in this url into result 
     doc = BeautifulSoup(result.text, "html.parser") # doc = html of that page 
 
@@ -36,7 +54,8 @@ def get_courses(url):
             #courseHTML = x.find_all(["a"], class_="bubblelink code")
             courseTitle = x.find_all(["a"], class_="bubblelink code")
             for y in courseTitle:
-                print(y.string.strip()) #TODO: put this output in a JSON file instead 
+                #print(y.string.strip()) #TODO: put this output in a JSON file instead 
+                f.write(y.string.strip())
 
     # for x in table:
     #     print(x)
@@ -53,7 +72,13 @@ def get_courses(url):
 
 def main():
     mainUrl = "https://guide.wisc.edu/undergraduate/letters-science/#degreesmajorscertificatestext" #Url to the page with all the majors 
-    get_majors_links(mainUrl)
+    major_link_pairs = get_majors_links(mainUrl)
+    keyList = major_link_pairs.keys()
+    #print(major_link_pairs)
+    for x in major_link_pairs:
+        finalLink = "https://guide.wisc.edu" + major_link_pairs[x] + "#requirementstext"
+        get_courses(x, finalLink)
+        #print(x, finalLink)
 
     #get_courses("https://guide.wisc.edu/undergraduate/letters-science/computer-sciences/computer-sciences-bs/index.html#requirementstext")
 
